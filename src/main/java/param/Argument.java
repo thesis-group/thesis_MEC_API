80d7aa3f6656b9c5427f14dbca0c6d698f3d0dd4
151250145 <151250145@smail.nju.edu.cn>
2018-01-26 10:04:16 +0800

    feat<adhoc>:add fot

--- src/main/java/param/Argument.java
+++ src/main/java/param/Argument.java
@@ -1,5 +1,7 @@
 package param;
 
 public class Argument {
-    public double sCpu; //本地执行所需要的cpu
+    public static double sCpu; //本地执行所需要的cpu add by local
+    public static double epsilon; //一个非常小但是大于0的值 add by adhoc
+    public static double fC; //单点执行失败率 add by adhoc
 }
--- src/main/java/reward/Strategy.java
+++ src/main/java/reward/Strategy.java
@@ -2,8 +2,12 @@ package reward;
 
 import model.Task;
 import param.AdHocParam;
+import param.Argument;
 import param.LocalParam;
 import param.RewardParam;
+import tools.Calculus;
+
+import java.util.function.Function;
 
 public enum Strategy implements StrategyService {
     // 本地卸载
@@ -29,8 +33,55 @@ public enum Strategy implements StrategyService {
             assert adHocParam.sad.size() != 0 : "adHocParam.sad cannot be empty";
             double sadSum = adHocParam.sad.stream().reduce((a, b) -> a + b).get();
             double eSpeed = adHocParam.ps * sadSum / adHocParam.l;
+            //计算超时失败率
+            double pHt = 2 * adHocParam.p2 * adHocParam.p3;
+            double pVt = 2 * adHocParam.p1 * adHocParam.p3 + 2 * adHocParam.p1 * adHocParam.p2;
+
+            //计算瑞利分布函数
+            Function<Double,Double> f =
+                    x -> (x*x/adHocParam.sigma)*Math.exp(-x*x/(2*adHocParam.sigma*adHocParam.sigma));
+
+            //通过自组织网的一次执行时间 这里原公式E_cp 就当做是E_speed了
+            double taoAd = adHocParam.delta * task.getIp() / adHocParam.rup
+                    + task.getWl() / eSpeed
+                    + adHocParam.delta *  task.getOp()/adHocParam.rdown;
+            //水平情况
+                //呈同一方向  假设v1和v2都是标量
+                double v = Math.abs(adHocParam.v1 - adHocParam.v2);
+                double finalV = v;
+                Function<Double, Double> T101 = x-> ((adHocParam.R - adHocParam.r) -x)/(finalV + Argument.epsilon);
+                double f0s = Calculus.Integrate(
+                        x->((T101.apply(x)-taoAd)/T101.apply(x)) * f.apply(x),
+                        0 ,
+                        adHocParam.r + adHocParam.R
+                );
+                //呈不同方向
+                v = adHocParam.v1 + adHocParam.v2;
+                double finalV1 = v;
+                Function<Double, Double> T102 = x-> ((adHocParam.R - adHocParam.r) +x)/(finalV1 + Argument.epsilon);
+                double f0d = Calculus.Integrate(
+                        x->((T102.apply(x)-taoAd)/T102.apply(x)) * f.apply(x),
+                        0 ,
+                        adHocParam.r + adHocParam.R
+                );
+            //垂直情况 d不知道什么 觉得可能还是x吧
+            v = Math.sqrt(adHocParam.v1*adHocParam.v1 + adHocParam.v2*adHocParam.v2);
+            double finalV2 = v;
+            Function<Double, Double> T11 = x-> ((adHocParam.R - adHocParam.r) +x)/(finalV2 + Argument.epsilon);
+            double f1 = Calculus.Integrate(
+                    x->((T11.apply(x)-taoAd)/T11.apply(x)) * f.apply(x),
+                    0 ,
+                    adHocParam.r + adHocParam.R
+            );
+            //综合的期望失败率
+            double pOf = pHt * (f0s+f0d) + pVt * f1;
+
+
+
+
 
             return null;
         }
+
     };
 }
