17dab75efcaa61e29e827f4893db2e25f8cec2ab
151250145 <151250145@smail.nju.edu.cn>
2018-01-26 11:56:19 +0800

    feat<adhoc> : add rtt, ecomp and cpay

--- src/main/java/param/Argument.java
+++ src/main/java/param/Argument.java
@@ -4,4 +4,6 @@ public class Argument {
     public static double sCpu; //本地执行所需要的cpu add by local
     public static double epsilon; //一个非常小但是大于0的值 add by adhoc
     public static double fC; //单点执行失败率 add by adhoc
+    public static double lambda; //泊松过程参数 add by adhoc
+    public static double er; //单位自组织网传递能耗 add by adhoc
 }
