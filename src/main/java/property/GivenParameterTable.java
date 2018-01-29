df54e038929c33717a65530e09b66e0554f89899
Green-Cherry <2423964104@qq.com>
2018-01-29 18:01:09 +0800

    fix<property>:change some values

--- src/main/java/property/GivenParameterTable.java
+++ src/main/java/property/GivenParameterTable.java
@@ -5,21 +5,21 @@ package property;
  */
 public class GivenParameterTable {
     //The weight value of energy consumption in 𝐶𝐿(𝒮)
-    public static double wL1 = 0.3;
+    public static double wL1 = 0;
     //The weight value of energy consumption in 𝐶𝐿(𝒮)
-    public static double wL2 = 0.3;
+    public static double wL2 = 0;
     //The weight value of failure rate in 𝐶𝐿(𝒮)
-    public static double wL3 = 0.4;
+    public static double wL3 = 0;
     //The weight value of failure rate in c𝐶𝐿(𝒮)
-    public static double wCL1 = 0.3;
+    public static double wCL1 = 0;
     //The weight value of payment in 𝐶𝐶𝐿(𝒮)
-    public static double wCL2 = 0.3;
+    public static double wCL2 = 0;
     //The weight value of failure rate in 𝐶𝐶𝐿(𝒮)
-    public static double wCL3 = 0.4;
+    public static double wCL3 = 0;
     //The weight value of failure rate in 𝐶𝐴𝐷(𝒮)
-    public static double wAD1 = 0.3;
+    public static double wAD1 = 0;
     //The weight value of payment in 𝐶𝐴𝐷(𝒮)
-    public static double wAD2 = 0.3;
+    public static double wAD2 = 0;
     //The weight value of failure rate in 𝐶𝐴𝐷(𝒮)
-    public static double wAD3 = 0.4;
+    public static double wAD3 = 0;
 }
